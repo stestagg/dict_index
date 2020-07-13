761d22299c9fd626d00255a01b8f1cbc36de2a92
Steve Stagg <stestagg@gmail.com>
2020-07-13 15:49:39 +0000

    Add big2 results

--- benchmark/base.py
+++ benchmark/base.py
@@ -9,9 +9,9 @@ def make_dict(n):
 
 class Test:
     _TEST_METHODS = set()
-    DICT_SIZES = {1, 10, 100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000}
-    NUM_RUNS = 10
     CHECK_RESULTS = True
+    DICT_SIZES = {1, 1_000, 100_000, 10_000_000, 100_000_000, 1_000_000_000}
+    NUM_RUNS = 3
 
     NAME = NotImplemented
 
